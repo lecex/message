baf76babff798d60bbf51a155596454d3420a065
bigrocs <bigrocs@qq.com>
2021-02-20 16:46:12 +0800

    自动提交 git 代码

--- service/sms/aliyun.go
+++ service/sms/aliyun.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 
-	"github.com/micro/go-micro/util/log"
+	"github.com/micro/go-micro/v2/util/log"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
